b0124b973c65b952ed53f2cacb7988e674917b33
samuelconlon <conlonsp@yahoo.com>
2023-01-06 16:26:36 -0500

    commit tree a03afd6a6dc764abb5a14e3f4c6ca44e255321d5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: ["Fiction", "Non-Fiction"]
+  validate :true_facts
+
+  def true_facts
+    if title == "True Facts"
+      errors.add(:title, "Not Valid Title")
+    end
+  end
 end
